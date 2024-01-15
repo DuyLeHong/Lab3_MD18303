478b630d4ee38b5a6643a74d50ccd1cddb66164a
Duy Le <lehongduy.bk@gmail.com>
2024-01-15 13:53:46 +0700

    update code

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -5,6 +5,58 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
 
+    Rigidbody2D rigidbody2D;
+    Transform transform;
+    SpriteRenderer spriteRenderer;
+    bool isGrounded = false;
+
+    private void Start()
+    {
+        rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+        transform = this.gameObject.GetComponent<Transform>();
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    float movePrefix = 6;
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rigidbody2D.AddForce(Vector2.up * movePrefix, ForceMode2D.Impulse);
+                isGrounded = false;
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //transform.localScale = new Vector3(-1, 1, 1);
+            spriteRenderer.flipX = true;
+
+
+            //transform.Translate(- Time.deltaTime * 100, 0, 0);
+
+            //Vector2 scale = transform.localScale;
+            //scale.x *= scale.x > 0 ? -1 : 1;
+            //// neu scale >0 thi scale lon hon 0 else
+            //transform.localScale = scale;
+
+            rigidbody2D.AddForce(Vector2.left * movePrefix, ForceMode2D.Impulse);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            spriteRenderer.flipX = false;
+            rigidbody2D.AddForce(Vector2.right * movePrefix, ForceMode2D.Impulse);
+            
+        }
+    }
+
+    
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "coin")
@@ -16,49 +68,11 @@ public class PlayerManager : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Va cham voi: " + other.gameObject.tag);
-    }
-
-    // Start is called before the first frame update
-    Rigidbody2D rigidbody2D;
-    Transform transform;
-
-    void Start()
-    {
-        rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
-        transform = this.gameObject.GetComponent<Transform>();
-    }
-
-    float movePrefix = 3;
-
-    float xMove = 0;
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        //xMove = Input.GetAxisRaw("Horizontal"); // an phim trai phai
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (other.gameObject.tag == "nendat")
         {
-            rigidbody2D.AddForce(Vector2.up * movePrefix, ForceMode2D.Impulse);
-            //rigidbody2D.velocity = Vector2.up * movePrefix;
-            //transform.Translate(Vector3.up * movePrefix);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            rigidbody2D.AddForce(Vector2.left * movePrefix, ForceMode2D.Impulse);
-
-            transform.localScale = new Vector3(-1, 1, 1);
-
-            //transform.Translate(Vector3.left * movePrefix);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            rigidbody2D.AddForce(Vector2.right * movePrefix, ForceMode2D.Impulse);
-
-            transform.localScale = new Vector3(1, 1, 1);
-
-            //transform.Translate(Vector3.right * movePrefix);
+            isGrounded = true;
         }
     }
+
+    
 }
